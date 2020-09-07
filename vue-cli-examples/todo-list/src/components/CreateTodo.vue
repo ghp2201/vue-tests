d1b0385a4354689fbe0b18496f2c68c72d0fdd23
ghp2201 <ghp2201@gmail.com>
2020-09-06 22:13:17 -0300

    Reformulate repo structure

---
+++ vue-cli-examples/todo-list/src/components/CreateTodo.vue
@@ -0,0 +1,80 @@
+<template>
+  <!--
+    The styles are provided by the Semantic UI framework
+  -->
+  <div class="ui basic content center aligned segment">
+    <button class="ui basic button icon" v-on:click="openForm" v-show="!isCreating">
+      <i class="plus icon"></i>
+    </button>
+
+    <div class="ui centered card" v-show="isCreating">
+
+      <div class="content">
+        <div class="ui form">
+          <div class="field">
+            <label>Title</label>
+            <input type="text" ref="title" defaultValue="" v-model="titleText">
+          </div>
+          <div class="field">
+            <label>Project</label>
+            <input type="text" ref="project" defaultValue="" v-model="projectText">
+          </div>
+          <div class="ui two button attached buttons">
+            <button class="ui basic blue button" v-on:click="sendForm()">
+              Create
+            </button>
+            <button class="ui basic red button" v-on:click="closeForm">
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      titleText: '',
+      projectText: '',
+      isCreating: false
+    }
+  },
+  methods: {
+    openForm () {
+      this.isCreating = true
+    },
+
+    closeForm () {
+      this.isCreating = false
+    },
+
+    sendForm () {
+      if (this.titleText.length > 0 && this.projectText.length > 0) {
+        const title = this.titleText
+        const project = this.projectText
+
+        /*
+          Emit a 'create-todo' event to App Parent
+        */
+        this.$emit('create-todo', {
+          title: title,
+          project: project,
+          done: false
+        })
+
+        this.titleText = ''
+        this.projectText = ''
+        this.isCreating = false
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
